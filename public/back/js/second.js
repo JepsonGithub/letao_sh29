81604fdb27e8044efa0d31d34289ecbfa2e2126e
jepson <jepsonpp@qq.com>
2018-11-16 18:07:42 +0800

    基本列表渲染完成, 文件上传功能完成

--- public/back/js/second.js
+++ public/back/js/second.js
@@ -3,7 +3,7 @@
  */
 $(function() {
 
-  // 一进入页面, 发送请求, 获取数据进行渲染
+  // 1. 一进入页面, 发送请求, 获取数据进行渲染
   var currentPage = 1; // 当前页
   var pageSize = 5; // 每页条数
   render();
@@ -23,9 +23,74 @@ $(function() {
         // 根据模板引擎进行渲染
         var htmlStr = template("secondTpl", info );
         $('tbody').html( htmlStr );
+
+        // 进行分页初始化
+        $('#paginator').bootstrapPaginator({
+          bootstrapMajorVersion: 3, // 版本号
+          totalPages: Math.ceil( info.total / info.size ),
+          currentPage: info.page,
+          onPageClicked: function( a, b, c, page ) {
+            // 更新当前页
+            currentPage = page;
+            // 重新渲染
+            render();
+          }
+        })
       }
     })
   }
+
+
+  // 2. 点击添加按钮, 显示添加模态框
+  $('#addBtn').click(function() {
+
+    // 显示添加模态框
+    $('#addModal').modal("show");
+
+    // 发送ajax请求, 获取下拉菜单的列表数据(全部的一级分类)
+    // 通过分页获取一级分类的接口, 模拟获取全部数据的接口, page=1, pageSize: 100
+    $.ajax({
+      type: "get",
+      url: "/category/queryTopCategoryPaging",
+      data: {
+        page: 1,
+        pageSize: 100
+      },
+      dataType: "json",
+      success: function( info ) {
+        console.log( info );
+        var htmlStr = template( "dropdownTpl", info );
+        $('.dropdown-menu').html( htmlStr );
+      }
+    })
+
+  });
+
+
+
+  // 3. 给下拉菜单的所有 a 添加点击事件, 通过事件委托注册
+  $('.dropdown-menu').on("click", "a", function() {
+    // 获取 a 的文本
+    var txt = $(this).text();
+    // 将文本设置给 按钮
+    $('#dropdownText').text( txt );
+  });
+
+
+  // 4. 进行文件上传初始化
+  $('#fileupload').fileupload({
+    dataType: "json",
+    // 表示文件上传完成的回调函数
+    done: function( e, data ) {
+      console.log( data );
+      // 后台返回的结果
+      var result = data.result;
+      // 获取文件上传的地址
+      var picUrl = result.picAddr;
+      // 设置给 img 的 src
+      $('#imgBox img').attr("src", picUrl );
+    }
+  })
 
 
 })
